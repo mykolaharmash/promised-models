ec0dc7e72bf4046f8d849c23b644b1a8387b5106
Vova Bilonenko <bilonenko.v@gmail.com>
2015-05-13 20:02:53 +0300

    Models.inherit

--- test/common.js
+++ test/common.js
@@ -1,14 +1,21 @@
-var Models = require('../lib/promised-models');
-require('chai').expect();
+var Models = require('../lib/promised-models'),
+    expect = require('chai').expect;
 
 describe('Models.inherit', function () {
-    var modelClass = Models.inherit({
+    var ModelClass = Models.inherit({
         fields: {
             a: {
                 type: 'string',
                 default: 'a'
             }
         }
+    });
+    it('should have inherit', function () {
+        expect(ModelClass.inherit).to.be.a('function');
+    });
+    it('should have fields', function () {
+        var model = new ModelClass();
+        expect(model).to.have.property('fields');
     });
 
 });
