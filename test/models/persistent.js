cde0ce99ec365ed8e650ee789040fba414d00ebd
Vova Bilonenko <bilonenko.v@gmail.com>
2015-05-19 20:34:56 +0300

    persistent

---
+++ test/models/persistent.js
@@ -0,0 +1,52 @@
+/**
+ * Presistant model
+ */
+var Model = require('../../lib/promised-models'),
+    Vow = require('vow'),
+    storage = [];
+
+module.exports = Model.inherit({
+    fields: {
+        a: Model.fields.String.inherit({
+            default: 'a-0'
+        }),
+        b: Model.fields.String.inherit({
+            calculate: function () {
+                var field = this;
+                return Vow.fulfill().delay(0).then(function () {
+                    return 'b-' + field.model.get('a').split('-')[1];
+                });
+            }
+        })
+    },
+    storage: Model.Storage.inherit({
+        insert: function (model) {
+            return Vow.fulfill().delay(0).then(function () {
+                storage.push(model.toJSON());
+                return storage.length - 1;
+            });
+        },
+        update: function (model) {
+            return Vow.fulfill().delay(0).then(function () {
+                storage[model.id] = model.toJSON();
+            });
+        },
+        find: function (model) {
+            return Vow.fulfill().delay(0).then(function () {
+                var data =  storage[model.id];
+                if (data) {
+                    return data;
+                } else {
+                    return Vow.reject(new Error('Not found'));
+                }
+            });
+        },
+        remove: function (model) {
+            return Vow.fulfill().delay(0).then(function () {
+                storage[model.id] = null;
+            });
+        }
+    })
+}, {
+    testStorage: storage
+});
--- test/persistent.js
+++ test/persistent.js
@@ -1,11 +1,62 @@
 
 var expect = require('chai').expect;
 
-describe('Presistant', function () {
-    var Model = require('./models/presistant');
-    it('should save and fetch model by id', function () {
+describe('Persistent', function () {
+    var Model = require('./models/persistent');
+    it('should save and fetch model by id', function (done) {
         var model1 = new Model();
         model1.set('a', 'a-2');
-        return model1.save();
+        model1.save().then(function () {
+            var model2 = new Model(model1.id);
+            return model2.fetch().then(function () {
+                expect(model2.get('a')).to.be.equal('a-2');
+                done();
+            });
+        }).done();
+    });
+    it('should insert and update calculated fields', function () {
+        var model1 = new Model();
+        model1.set('a', 'a-2');
+        return model1.save().then(function () {
+            expect(Model.testStorage[model1.id]).to.have.property('a', 'a-2');
+            expect(Model.testStorage[model1.id]).to.have.property('b', 'b-2');
+            model1.set('a', 'a-3');
+            return model1.save();
+        }).then(function () {
+            expect(Model.testStorage[model1.id]).to.have.property('a', 'a-3');
+            expect(Model.testStorage[model1.id]).to.have.property('b', 'b-3');
+        });
+    });
+    it('should update', function (done) {
+        var model1 = new Model();
+        model1.set('a', 'a-1');
+        model1.save().then(function () {
+            var model2 = new Model(model1.id);
+            return model2.fetch().then(function () {
+                expect(model2.get('a')).to.be.equal('a-1');
+                model2.set('a', 'a-2');
+                return model2.save();
+            }).then(function () {
+                return model1.fetch();
+            }).then(function () {
+                expect(model1.get('a')).to.be.equal('a-2');
+                done();
+            });
+        }).done();
+    });
+    it('should delete', function (done) {
+        var model1 = new Model({
+            a: 'a-3'
+        });
+        expect(model1.get('a')).to.be.equal('a-3');
+        model1.save().then(function () {
+            var id = model1.id;
+            return model1.remove().then(function () {
+                var model2 = new Model(id);
+                return model2.fetch().fail(function () {
+                    done();
+                });
+            });
+        }).done();
     });
 });
