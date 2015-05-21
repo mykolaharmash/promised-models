3b828549fb44c289ea3a5aa315f6da6d552ca5a8
Vova Bilonenko <bilonenko.v@gmail.com>
2015-05-21 13:04:39 +0300

    nestyd async models

--- test/models/with-nested.js
+++ test/models/with-nested.js
@@ -5,7 +5,12 @@ var Model = require('../../lib/promised-models'),
     NestedModel = Model.inherit({
         fields: {
             a: Model.fields.String,
-            b: Model.fields.String
+            b: Model.fields.String,
+            invalid: Model.fields.Number.inherit({
+                validate: function () {
+                    return Boolean(this.value);
+                }
+            })
         }
     });
 
@@ -13,6 +18,9 @@ module.exports = Model.inherit({
     fields: {
         nested: Model.fields.Model.inherit({
             modelType: NestedModel
+        }),
+        nestedAsync: Model.fields.Model.inherit({
+            modelType: require('./with-calculations')
         })
     }
 });
--- test/nested-models.js
+++ test/nested-models.js
@@ -8,14 +8,15 @@ describe('Nested models', function () {
             data = {
                 nested: {
                     a: 'a-0',
-                    b: 'b-0'
+                    b: 'b-0',
+                    invalid: 0
                 }
             };
             model = new Model(data);
-
+            return model.ready();
         });
         it('should serialize to toJSON', function () {
-            expect(model.toJSON()).to.be.deep.equal(data);
+            expect(model.toJSON().nested).to.be.deep.equal(data.nested);
         });
         it('should trigger change on parent model', function (done) {
             model.on('change', function () {
@@ -28,6 +29,34 @@ describe('Nested models', function () {
                 done();
             });
             model.get('nested').set('a', 'a-1');
+        });
+        it('should validate', function (done) {
+            model.validate().fail(function () {
+                model.get('nested').set('invalid', 1);
+                return model.validate().then(function () {
+                    done();
+                });
+            }).done();
+        });
+        it('isChanged should be false after set same instance', function () {
+            model.set('nested', model.get('nested'));
+            return model.ready().then(function () {
+                expect(model.isChanged()).to.be.equal(false);
+            }).done();
+        });
+        it('isChanged should be true if nested changed', function () {
+            model.get('nested').set('a', 'a-1');
+            expect(model.isChanged()).to.be.equal(true);
+        });
+        it('isChanged should be false after revert', function () {
+            model.get('nested').set('a', 'a-1');
+            model.revert();
+            expect(model.isChanged()).to.be.equal(false);
+        });
+        it('isChanged should be false after commit', function () {
+            model.get('nested').set('a', 'a-1');
+            model.commit();
+            expect(model.isChanged()).to.be.equal(false);
         });
     });
 });
